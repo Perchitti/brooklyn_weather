3cea38c34dedf6a0ac996b6fd38bc12ea15e4b41
Perchitti <LaurenPerchitti@gmail.com>
2018-04-30 09:08:57 -0400

    Enviornment

--- lib/brooklyn_weather.rb
+++ lib/brooklyn_weather.rb
@@ -1,17 +1,15 @@
 #this code acts as the enviornment
 require 'open-uri'
-#require 'pry'
-#require 'nokogiri'
-
-#WHY DOESNT It ALLOW NOKOGIRI FROM HERE?
 
 
-#requiring version
+
+
+
 require_relative './brooklyn_weather/version'
 
-#requiring cli
+
 require_relative './brooklyn_weather/cli'
 
 
-#requiring scraper
+
 require_relative './brooklyn_weather/scraper'
